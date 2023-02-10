2e5eb18f17e6bbaa37c70fe5d3fa80f974b88044
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-10 11:31:55 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import 'antd/dist/reset.css';
 import {
   Button,
@@ -9,24 +7,10 @@ import {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    <div className="app">
-      <Input placeholder="请输入" />
-      <Button type='primary'>按钮</Button>
-    </div>
+      <div className="app">
+        <Input placeholder="请输入" />
+        <Button type='primary'>按钮</Button>
+      </div>
     </div>
   );
 }
