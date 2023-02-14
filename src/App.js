73fdeeaf288188148d86497899b0559324a64d36
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-14 14:07:46 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import {
   Input
 } from 'antd'
 
+import { loginService } from './service/login/login';
+
+const user = {userId: "admin", password: "admin"}
+
 function App() {
   return (
     <div className="App">
       <div className="app">
-        <Input placeholder="请输入" />
-        <Button type='primary'>按钮</Button>
+        <Input placeholder="请输入"  defaultValue={process.env.BASE_URL}  />
+        <Button type='primary' onClick={ () => {console.log(process.env);loginService(user)} }>按钮</Button>
       </div>
     </div>
   );
