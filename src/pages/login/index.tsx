e96f51e6cf1c6841c877f6a8e401a1e7d960f90d
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-15 10:12:38 +0800

    Create index.tsx

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+
+
+interface LoginFormProps {
+  title: string;
+  modalVisible: boolean;
+  handleOk: any;
+  handleCancel: any;
+  data: any;
+}
+
+const LoginForm: React.FC<LoginFormProps> = props => {
+  const { title, modalVisible, handleOk, handleCancel, data } = props;
+
+  const [user, setUser] = useState({
+    name: 'admin',
+    password: 'admin',
+  });
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+  
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div id='loginForm'>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default LoginForm;
