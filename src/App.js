e8fbc64c4184a47aceb3a2dc17b76c2f1ec6ea17
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-15 10:11:14 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import 'antd/dist/reset.css';
-import {
-  Button,
-  Input
-} from 'antd'
-
-import { loginService } from './service/login/login';
-
-const user = {userId: "admin", password: "admin"}
+import { useRoutes } from 'react-router-dom'
+import routes from './router'
 
 function App() {
-  return (
-    <div className="App">
-      <div className="app">
-        <Input placeholder="请输入"  defaultValue={process.env.BASE_URL}  />
-        <Button type='primary' onClick={ () => {console.log(process.env);loginService(user)} }>按钮</Button>
-      </div>
-    </div>
-  );
+  const elements = useRoutes(routes);
+  return elements;
 }
 
 export default App;
