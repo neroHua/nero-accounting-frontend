bd697e3ad037c29f6384172bdebe1f07a23b26e2
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-22 17:39:25 +0800

    Create index.tsx

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,24 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import React, { useState } from 'react';
+import { Button, Form, Input } from 'antd';
+import { loginService} from '../../service/login/login.js';
 
-
-interface LoginFormProps {
-  title: string;
-  modalVisible: boolean;
-  handleOk: any;
-  handleCancel: any;
-  data: any;
-}
-
-const LoginForm: React.FC<LoginFormProps> = props => {
-  const { title, modalVisible, handleOk, handleCancel, data } = props;
+const LoginForm: React.FC = props => {
 
   const [user, setUser] = useState({
-    name: 'admin',
+    userId: 'admin',
     password: 'admin',
   });
 
   const onFinish = (values: any) => {
+    loginService(values);
     console.log('Success:', values);
   };
   
@@ -33,34 +25,30 @@ const LoginForm: React.FC<LoginFormProps> = props => {
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 16 }}
         style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
+        initialValues={ user }
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
         autoComplete="off"
       >
         <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: 'Please input your username!' }]}
+          label="账号"
+          name="userId"
+          rules={[{ required: true, message: '请输入账号！' }]}
         >
           <Input />
         </Form.Item>
 
         <Form.Item
-          label="Password"
+          label="密码"
           name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
+          rules={[{ required: true, message: '请输入密码！' }]}
         >
           <Input.Password />
         </Form.Item>
 
-        <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
           <Button type="primary" htmlType="submit">
-            Submit
+            登录
           </Button>
         </Form.Item>
       </Form>
