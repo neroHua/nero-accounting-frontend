efd0c9a821563cb87a4a471ccd2d1991343b4cd7
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-23 15:04:53 +0800

    Create App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import 'antd/dist/reset.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 import routes from './router'
 
 function App() {
   return (
     <BrowserRouter>
+      <NavLink to="/login">登录</NavLink>
+      <NavLink to="/tag">标签</NavLink>
+      <NavLink to="/accounting">账单</NavLink>
       <Routes>
         {routes.map((item, index) => {
             return (
