3f4860780842c4ec3841c77f7bba6362578978a6
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-23 14:37:42 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,19 @@
 import LoginForm from "../pages/login/index.tsx"
+import TagPage from "../pages/tag/index.tsx"
 
 const routes = [
   {
     path: "/",
     redirect: '/login',
+    element: LoginForm,
   },
   {
     path: "/login",
-    element: <LoginForm/>
+    element: LoginForm,
+  },
+  {
+    path: "/tag",
+    element: TagPage,
   }
 ]
 
