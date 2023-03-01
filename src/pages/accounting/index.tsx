ed5714cb525096fda91b9483df71bd12151ad50f
neroHua <38644203+neroHua@users.noreply.github.com>
2023-03-01 10:56:15 +0800

    Update index.tsx

--- src/pages/accounting/index.tsx
+++ src/pages/accounting/index.tsx
@@ -1,44 +1,274 @@
-import React from 'react';
-import { Table } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Table, Button } from 'antd';
+import {
+  accountingGetByPageService,
+  accountingAddService,
+  accountingUpdateService,
+  accountingDeleteService,
+  accountingTagAddService,
+  accountingTagDeleteService,
+  accountingTagGetByPageService
+} from '../../service/accounting/accounting.js';
+import { ValuableEnumeration } from '../../enumeration/accounting/accounting';
+import { useLocation } from 'react-router-dom';
+import querystring from "query-string";
+import moment from 'moment';
+// @ts-ignore
+import AccountingUpdate from './component/accountingUpdate.tsx'
+// @ts-ignore
+import AccountingAdd from './component/accountingAdd.tsx'
+// @ts-ignore
+import AccountingSearch from './component/accountingSearch.tsx'
 
-const AccountingPage: React.FC = props => {
+const AccountingPage: React.FC<any> = () => {
+  let location = useLocation();
 
-  const dataSource = [
-    {
-      key: '1',
-      name: '胡彦斌1',
-      age: 32,
-      address: '西湖区湖底公园1号',
-    },
-    {
-      key: '2',
-      name: '胡彦祖1',
-      age: 42,
-      address: '西湖区湖底公园1号',
-    },
-  ];
-  
+  const [pagination , setPagination] = useState<any>({
+    current: 1,
+    pageSize: 10,
+    total: 0,
+    pageSizeOptions: [10, 20, 30, 50, 100],
+  });
+  const [accountingSearch , setAccountingSearch] = useState<any>({
+    values: {},
+  });
+  const [accountingList , setAccountingList] = useState<any[]>([]);
+
+  const getAccountingList = async (request : any) => {
+    const data = await accountingGetByPageService({
+      ...request,
+      codeEqual: request.codeEqual ? request.codeEqual : null,
+      nameEqual: request.nameEqual ? request.nameEqual : null,
+      parentId: request.parentId ? request.parentId : null,
+    })
+
+    setAccountingList(data?.dataList || []);
+
+    setPagination({
+      ...pagination,
+      current: request.pageNumber,
+      pageSize: request.pageSize,
+      total: data.totalCount,
+    });
+    setAccountingSearch({
+      values: {
+        codeEqual: request.codeEqual,
+        nameEqual: request.nameEqual,
+        parentId: request.parentId,
+      }
+    });
+  };
+
+  const [accountingAdd , setAccountingAdd] = useState<any>({
+    visible: false,
+    values: {},
+  });
+
+  const accountingAddShow = (record : any) => {
+    setAccountingAdd({
+      visible : true,
+      values: {
+        ...record,
+      }
+    });
+  }
+
+  const accountingAddSubmit = async (values : any) => {
+    const data = await accountingAddService({...values});
+    if (data?.success) {
+      return;
+    }
+
+    setAccountingAdd({
+      visible : false,
+      values: {}
+    });
+    getAccountingList({pageNumber: pagination.current, pageSize: pagination.pageSize, ...accountingSearch.values});
+  }
+
+  const accountingDelete = async (record : any) => {
+    const data = await accountingDeleteService(record.id);
+    if (data?.success) {
+      return;
+    }
+    getAccountingList({pageNumber: pagination.current, pageSize: pagination.pageSize, ...accountingSearch.values});
+  }
+
+  const [accountingUpdate , setAccountingUpdate] = useState<any>({
+    visible: false,
+    values: {},
+  });
+
+  const accountingUpdateShow = (record : any) => {
+    setAccountingUpdate({
+      visible : true,
+      values: {
+        ...record,
+        createTime: moment(record.createTime),
+        updateTime: moment(record.updateTime),
+        billCreateTime: moment(record.billCreateTime),
+      }
+    });
+  }
+
+  const accountingUpdateSubmit = async (values : any) => {
+    const data = await accountingUpdateService({...values});
+    if (data?.success) {
+      return;
+    }
+
+    setAccountingUpdate({
+      visible : false,
+      values: {}
+    });
+    getAccountingList({pageNumber: pagination.current, pageSize: pagination.pageSize, ...accountingSearch.values});
+  }
+
+  useEffect(() => {
+    const {pageNumber = 1, pageSize = 10, codeEqual, nameEqual, parentId} = querystring.parse(location.search);
+    getAccountingList({pageNumber, pageSize, codeEqual, nameEqual, parentId});
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
   const columns = [
     {
-      title: '姓名',
-      dataIndex: 'name',
-      key: 'name',
+      title: 'id',
+      dataIndex: 'id',
     },
     {
-      title: '年龄',
-      dataIndex: 'age',
-      key: 'age',
+      title: '创建时间',
+      dataIndex: 'createTime',
+      render: (text, record, index) => {
+        return moment(record.createTime).format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS);
+      },
     },
     {
-      title: '住址',
-      dataIndex: 'address',
-      key: 'address',
+      title: '修改时间',
+      dataIndex: 'updateTime',
+      render: (text, record, index) => {
+        return moment(record.updateTime).format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS);
+      },
+    },
+    {
+      title: '创建者id',
+      dataIndex: 'createUserId',
+    },
+    {
+      title: '更新者id',
+      dataIndex: 'updateUserId',
+    },
+    {
+      title: '账单用户',
+      dataIndex: 'userId',
+    },
+    {
+      title: '账单创建时间',
+      dataIndex: 'billCreateTime',
+      render: (text, record, index) => {
+        return moment(record.billCreateTime).format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS);
+      },
+    },
+    {
+      title: '账单金额',
+      dataIndex: 'billMoney',
+    },
+    {
+      title: '描述',
+      dataIndex: 'description',
+    },
+    {
+      title: '复盘描述',
+      dataIndex: 'reviewDescription',
+    },
+    {
+      title: '是否值得',
+      dataIndex: 'valuable',
+      render: (text, record, index) => {
+        const valuable = ValuableEnumeration.filter((item : any) => {
+          return item.value === record.valuable;
+        });
+        return valuable[0].label || '-';
+      },
+    },
+    {
+      title: '操作',
+      dataIndex: 'operation',
+      render: (text, record, index) => {
+        return (<div>
+          <Button
+            type="primary"
+            onClick={(e) => {
+              accountingUpdateShow(record);
+            }}
+          >
+            修改
+          </Button>
+          <Button
+            type="primary"
+            danger
+            onClick={(e) => {
+              accountingDelete(record);
+            }}
+          >
+            删除
+          </Button>
+        </div>);
+      },
     },
   ];
 
   return (
     <div>
-      <Table dataSource={dataSource} columns={columns} />;
+      <div style={{display: 'flex'}}>
+      <AccountingSearch
+        onSubmit={(values) => {
+          getAccountingList({...values, pageNumber: pagination.current, pageSize: pagination.pageSize});
+        }}
+        values={accountingSearch.values}
+      >
+      </AccountingSearch>
+      <Button
+        type='primary'
+        onClick={(e) => {
+          accountingAddShow({});
+        }}
+      >
+        新增
+      </Button>
+      </div>
+
+      <Table
+        dataSource={accountingList}
+        rowKey={'id'}
+        columns={columns}
+        pagination={pagination}
+        onChange={(pagination) => {
+          getAccountingList({
+            pageSize: pagination.pageSize,
+            pageNumber: pagination.current
+          });
+        }}
+      />;
+
+      <AccountingAdd
+        visible={accountingAdd.visible}
+        onCancel={() => {
+          setAccountingAdd({...accountingAdd, visible: false});
+        }}
+        onSubmit={accountingAddSubmit}
+        values={accountingAdd.values}
+      >
+      </AccountingAdd>
+
+      <AccountingUpdate
+        visible={accountingUpdate.visible}
+        onCancel={() => {
+          setAccountingUpdate({...accountingUpdate, visible: false});
+        }}
+        onSubmit={accountingUpdateSubmit}
+        values={accountingUpdate.values}
+      >
+      </AccountingUpdate>
     </div>
   );
 };
