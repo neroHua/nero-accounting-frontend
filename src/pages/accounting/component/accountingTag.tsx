52fe9cb0c68ad72a293e31c282a1b224c6367068
neroHua <38644203+neroHua@users.noreply.github.com>
2023-04-07 16:55:55 +0800

    Create accountingTag.tsx

--- src/pages/accounting/component/accountingTag.tsx
+++ src/pages/accounting/component/accountingTag.tsx
@@ -42,7 +42,6 @@ const AccountingTag : React.FC<AccountingTagProps> = props => {
   }
 
   const accountingTagAddSubmit = async (values : any) => {
-    console.log(id);
     const data = await accountingTagAddService({accountingId : id, tagId: values.tagId});
     if (data?.success) {
       return;
@@ -64,7 +63,6 @@ const AccountingTag : React.FC<AccountingTagProps> = props => {
   }
 
   useEffect(() => {
-    console.log(id);
     getAccountingTagList({ id });
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
@@ -114,7 +112,7 @@ const AccountingTag : React.FC<AccountingTagProps> = props => {
       forceRender={true}
       width={800}
     >
-      <div style={{display: 'flex'}}>
+      <div>
         <Button
           type='primary'
           onClick={(e) => {
@@ -122,6 +120,342 @@ const AccountingTag : React.FC<AccountingTagProps> = props => {
           }}
         >
           新增
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 1});
+          }}
+        >
+          食物
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 2});
+          }}
+        >
+          娱乐
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 3});
+          }}
+        >
+          衣物
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 4});
+          }}
+        >
+          居住
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 5});
+          }}
+        >
+          交通
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 6});
+          }}
+        >
+          医疗
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 7});
+          }}
+        >
+          社交
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 8});
+          }}
+        >
+          次坞打面
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 9});
+          }}
+        >
+          午餐
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 10});
+          }}
+        >
+          饮料
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 11});
+          }}
+        >
+          地铁
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 12});
+          }}
+        >
+          川菜
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 13});
+          }}
+        >
+          奶茶
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 14});
+          }}
+        >
+          汉堡
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 15});
+          }}
+        >
+          肉蟹煲
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 16});
+          }}
+        >
+          家用网络
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 17});
+          }}
+        >
+          大排面
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 18});
+          }}
+        >
+          公交
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 19});
+          }}
+        >
+          晚餐
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 20});
+          }}
+        >
+          卷膜
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 21});
+          }}
+        >
+          高铁
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 22});
+          }}
+        >
+          麻辣烫
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 23});
+          }}
+        >
+          水果
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 24});
+          }}
+        >
+          水
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 25});
+          }}
+        >
+          打车
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 26});
+          }}
+        >
+          烤鱼
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 27});
+          }}
+        >
+          洗浴
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 28});
+          }}
+        >
+          共享单车
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 29});
+          }}
+        >
+          南昌拌粉
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 30});
+          }}
+        >
+          话费
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 31});
+          }}
+        >
+          早餐
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 32});
+          }}
+        >
+          洗衣服
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 33});
+          }}
+        >
+          洗头
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 34});
+          }}
+        >
+          刀削面
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 35});
+          }}
+        >
+          礼物
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 36});
+          }}
+        >
+          日用
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 37});
+          }}
+        >
+          冷饮
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 38});
+          }}
+        >
+          方便面
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 39});
+          }}
+        >
+          理发
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 40});
+          }}
+        >
+          租房
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 41});
+          }}
+        >
+          鞋子
+        </Button>
+        <Button
+          type='primary'
+          onClick={(e) => {
+            accountingTagAddSubmit({ tagId: 42});
+          }}
+        >
+          帽子
         </Button>
       </div>
 
